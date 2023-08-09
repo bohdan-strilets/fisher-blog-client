7fa37139beedc698f10b193d113318d18b9b4766
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-08-09 20:36:51 +0200

    Added interceptors for req.

--- src/api/index.ts
+++ src/api/index.ts
@@ -18,4 +18,35 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      const originalRequest = error.config;
+
+      originalRequest._isRetry = true;
+      try {
+        const { data } = await api.get("api/v1/users/refresh-user");
+        const dataToLS = {
+          token: data.tokens.accessToken,
+          _persist: '{"version":-1,"rehydrated":true}',
+        };
+
+        localStorage.setItem("persist:user", JSON.stringify(dataToLS));
+        return api.request(originalRequest);
+      } catch (error) {
+        console.log("NOT AUTHORIZED");
+      }
+    }
+
+    throw error;
+  }
+);
+
 export default api;
